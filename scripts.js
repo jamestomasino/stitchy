26250948b46c1aefa88b890873b6195b3b390e09
James Tomasino <james@tomasino.org>
2019-12-28 22:35:10 +0000

    using konva

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-/* global FileReader, Image */
+/* global FileReader, Image, Konva */
 var imageLoader = document.getElementById('imageLoader')
-var canvas = document.getElementById('imageCanvas')
-var ctx = canvas.getContext('2d')
+var imageInput = document.getElementById('imageInput')
+var container = document.getElementById('container')
 
 imageLoader.addEventListener('change', handleImage, false)
 
@@ -10,11 +10,47 @@ function handleImage (e) {
   reader.onload = function (event) {
     var img = new Image()
     img.onload = function () {
-      canvas.width = img.width
-      canvas.height = img.height
-      ctx.drawImage(img, 0, 0)
+      var yoda = new Konva.Image({
+        x: 50,
+        y: 50,
+        image: img,
+        width: 106,
+        height: 118,
+        draggable: true
+      })
+      imageInput.classList.add('hide')
+      container.classList.add('active')
+      layer.add(yoda)
     }
     img.src = event.target.result
   }
   reader.readAsDataURL(e.target.files[0])
 }
+
+var width = window.innerWidth
+var height = window.innerHeight
+
+var stage = new Konva.Stage({
+  container: 'container',
+  width: width,
+  height: height
+})
+
+var layer = new Konva.Layer()
+stage.add(layer)
+
+stage.on('click tap', function (e) {
+  if (e.target === stage) {
+    stage.find('Transformer').destroy()
+    layer.draw()
+    return
+  }
+  if (!e.target.hasName('yoda')) {
+    return
+  }
+  stage.find('Transformer').destroy()
+  var tr = new Konva.Transformer()
+  layer.add(tr)
+  tr.attachTo(e.target)
+  layer.draw()
+})
