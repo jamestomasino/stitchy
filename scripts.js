c07af8468a09779583701da9b0330c044154bf5c
James Tomasino <james@tomasino.org>
2019-12-29 01:19:27 +0000

    set sizes with form

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
 /* global FileReader, Image, Konva, Blob */
+var imageForm = document.getElementById('imageForm')
 var imageLoader = document.getElementById('imageLoader')
-var imageInput = document.getElementById('imageInput')
+var form = document.getElementById('form')
 var container = document.getElementById('container')
+var rows = document.getElementById('rows')
+var columns = document.getElementById('columns')
 var width = window.innerWidth
 var height = window.innerHeight
 
@@ -9,111 +12,118 @@ var height = window.innerHeight
 var gridWidth = width - 15
 var gridHeight = height - 5
 
-// How many divisions will we use?
-var gw = 57
-var gh = 65
+form.addEventListener('submit', process)
 
-// Calculate the increments we can use to make that many lines
-var hInc = Math.floor(gridWidth / gw + 1)
-var vInc = Math.floor(gridHeight / gh + 1)
+function process (e) {
+  e.preventDefault()
+  form.classList.add('hide')
+  imageForm.classList.add('active')
 
-// Use the smaller increment size to keep the grid square and still fit
-if (hInc > vInc) hInc = vInc
-else vInc = hInc
+  // How many divisions will we use?
+  var gw = columns.value
+  var gh = rows.value
 
-// Multiply it back out so we have the actual grid dimensions
-var gridMaxWidth = hInc * gw + 1
-var gridMaxHeight = vInc * gh + 1
+  // Calculate the increments we can use to make that many lines
+  var hInc = Math.floor(gridWidth / gw + 1)
+  var vInc = Math.floor(gridHeight / gh + 1)
 
-var stage = new Konva.Stage({
-  container: 'container',
-  width: gridMaxWidth + 1,
-  height: gridMaxHeight + 1
-})
+  // Use the smaller increment size to keep the grid square and still fit
+  if (hInc > vInc) hInc = vInc
+  else vInc = hInc
 
-var layer = new Konva.Layer()
-stage.add(layer)
-layer.draw()
+  // Multiply it back out so we have the actual grid dimensions
+  var gridMaxWidth = hInc * gw + 1
+  var gridMaxHeight = vInc * gh + 1
 
-// Draw all the lines across the horizontal axis
-var hArray = []
-for (var i = 1; i <= gridMaxWidth; i += hInc) {
-  hArray.push(i, 1)
-  hArray.push(i, gridMaxHeight - 1)
-  hArray.push(i, 1)
-}
-
-// Draw all the lines across the vertical axis
-var vlines = new Konva.Line({
-  points: hArray,
-  stroke: 'blue',
-  strokeWidth: 0.1,
-  lineCap: 'round',
-  lineJoin: 'round'
-})
-
-var vArray = []
-for (i = 1; i <= gridMaxHeight; i += vInc) {
-  vArray.push(1, i)
-  vArray.push(gridMaxWidth - 1, i)
-  vArray.push(1, i)
-}
-var hlines = new Konva.Line({
-  points: vArray,
-  stroke: 'blue',
-  strokeWidth: 0.1,
-  lineCap: 'round',
-  lineJoin: 'round'
-})
-
-layer.add(vlines)
-layer.add(hlines)
-
-/* ************************************************************************ */
-
-/* Upload Image */
-imageLoader.addEventListener('change', handleImage, false)
-function handleImage (e) {
-  var reader = new FileReader()
-  reader.onload = function (event) {
-    var img = new Image()
-    img.onload = function () {
-      var yoda = new Konva.Image({
-        x: 50,
-        y: 50,
-        name: 'yoda',
-        image: img,
-        width: 106,
-        height: 118,
-        draggable: true
-      })
-      imageInput.classList.add('hide')
-      container.classList.add('active')
-      layer.add(yoda)
-      yoda.moveToBottom()
-      layer.batchDraw()
-    }
-    img.src = event.target.result
-  }
-  reader.readAsDataURL(e.target.files[0])
-}
-
-stage.on('click tap', function (e) {
-  if (e.target === stage) {
-    stage.find('Transformer').destroy()
-    layer.draw()
-    return
-  }
-  if (!e.target.hasName('yoda')) {
-    return
-  }
-  stage.find('Transformer').destroy()
-  var tr = new Konva.Transformer({
-    node: e.target,
-    keepRatio: true,
-    enabledAnchors: ['top-left', 'top-right', 'bottom-left', 'bottom-right']
+  var stage = new Konva.Stage({
+    container: 'container',
+    width: gridMaxWidth + 1,
+    height: gridMaxHeight + 1
   })
-  layer.add(tr)
-  layer.draw()
-})
 
+  var layer = new Konva.Layer()
+  stage.add(layer)
+  layer.draw()
+
+  // Draw all the lines across the horizontal axis
+  var hArray = []
+  for (var i = 1; i <= gridMaxWidth; i += hInc) {
+    hArray.push(i, 1)
+    hArray.push(i, gridMaxHeight - 1)
+    hArray.push(i, 1)
+  }
+
+  // Draw all the lines across the vertical axis
+  var vlines = new Konva.Line({
+    points: hArray,
+    stroke: 'blue',
+    strokeWidth: 0.1,
+    lineCap: 'round',
+    lineJoin: 'round'
+  })
+
+  var vArray = []
+  for (i = 1; i <= gridMaxHeight; i += vInc) {
+    vArray.push(1, i)
+    vArray.push(gridMaxWidth - 1, i)
+    vArray.push(1, i)
+  }
+  var hlines = new Konva.Line({
+    points: vArray,
+    stroke: 'blue',
+    strokeWidth: 0.1,
+    lineCap: 'round',
+    lineJoin: 'round'
+  })
+
+  layer.add(vlines)
+  layer.add(hlines)
+
+  /* ************************************************************************ */
+
+  /* Upload Image */
+  imageLoader.addEventListener('change', handleImage, false)
+  function handleImage (e) {
+    var reader = new FileReader()
+    reader.onload = function (event) {
+      var img = new Image()
+      img.onload = function () {
+        var yoda = new Konva.Image({
+          x: 50,
+          y: 50,
+          name: 'yoda',
+          image: img,
+          width: 106,
+          height: 118,
+          draggable: true
+        })
+        container.classList.add('active')
+        imageForm.classList.remove('active')
+        layer.add(yoda)
+        yoda.moveToBottom()
+        layer.batchDraw()
+      }
+      img.src = event.target.result
+    }
+    reader.readAsDataURL(e.target.files[0])
+  }
+
+  stage.on('click tap', function (e) {
+    if (e.target === stage) {
+      stage.find('Transformer').destroy()
+      layer.draw()
+      return
+    }
+    if (!e.target.hasName('yoda')) {
+      return
+    }
+    stage.find('Transformer').destroy()
+    var tr = new Konva.Transformer({
+      node: e.target,
+      keepRatio: true,
+      enabledAnchors: ['top-left', 'top-right', 'bottom-left', 'bottom-right']
+    })
+    layer.add(tr)
+    layer.draw()
+  })
+}
